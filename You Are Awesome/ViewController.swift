68b99fb8767808d2a24182fd6405a2ea79239934
Vincent Stephen Huang <hvincentstephen@vincents-mbp.bc.edu>
2017-09-02 10:08:43 -0400

    Completed work for Week 2

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -9,17 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
+    
+    
+    // This code executes when the view controller loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        
+        let messages = ["You are fantastic!",
+                        "You are great!",
+                        "You are amazing!",
+                        "When the Genius Bar needs help, they call you!",
+                        "You brigten my day!",
+                        "You are da bomb!",
+                        "I can't wait to use your app!",
+                        "Fabulous? That's you!"]
+        
+        
+        var newIndex = -1
+        
+        repeat {
+           newIndex = Int(arc4random_uniform(UInt32(messages.count)))
+        } while index == newIndex
+        
+        index = newIndex
+        messageLabel.text = messages[index]
+        
+//        var randomIndex = Int(arc4random_uniform(UInt32(messages.count)))
+//        messageLabel.text = messages[randomIndex]
+//
+        
+        
+        
+//        if index == messages.count {
+//            index = 0
+//        }
+//        
+//
+//        messageLabel.text = messages[index]
+//        index = index + 1
+//
+        
+        
+        
+        
+        
+//        let message1 = "You Are Fantastic!!!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
     }
-
-
+    
 }
 
